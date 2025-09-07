1c060dced8b69621bedb4fe471e0b644d081aed1
imon-n <imon.eeecu@gmail.com>
2025-09-07 15:40:51 +0600

    recomended products section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../components/Home/Banner";
 import CustomerLoved from "../components/Home/CustomerLoved";
+import RecomenderProduct from "../components/Home/RecomenderProduct";
 
 export default function Home() {
   return (
     <div  className="space-y-4 sm:space-y-6 md:space-y-8 lg:space-y-16">
       <Banner />
       <CustomerLoved />
+      <RecomenderProduct />
     </div>
   );
 }
