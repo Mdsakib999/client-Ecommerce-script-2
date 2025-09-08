3f8455665647d3fd93b3396c42d57389647e9d29
MuntasirMahmud <muntasirm525@gmail.com>
2025-09-08 18:21:11 +0600

    responsive issues fixed on about us page

--- src/components/shared/AboutSection.jsx
+++ src/components/shared/AboutSection.jsx
@@ -9,8 +9,8 @@ export default function AboutSection({
   reverse,
 }) {
   return (
-    <div className="flex gap-10 text-left mb-32">
-      <div className={`about-content flex flex-col justify-center gap-4 p-4 w-3/5 ${reverse ? "order-2" : "order-1"}`}>
+    <div className="flex flex-col md:flex-row  gap-10 text-left mb-32">
+      <div className={`about-content flex flex-col justify-center gap-4 p-4 md:w-3/5 w-full ${reverse ? "order-2" : "order-1"}`}>
         <h6 className="text-blue-500">{preheading}</h6>
         <h1 className="text-3xl font-semibold">{heading}</h1>
         <p className="w-4/5 opacity-70">{normaltext}</p>
@@ -24,7 +24,7 @@ export default function AboutSection({
           </div>
         )}
       </div>
-      <div className={`image-content w-2/5 ${reverse ? "order-1" : "order-2"}`}>
+      <div className={`image-content w-1/2 md:w-2/5 mx-auto ${reverse ? "order-1" : "order-2"}`}>
         <img src={image} className="w-100% rounded-lg" alt="history-image" />
       </div>
     </div>
--- src/components/shared/Info1.jsx
+++ src/components/shared/Info1.jsx
@@ -1,7 +1,7 @@
 import Countup from '../shared/Countup'
 export default function Info1() {
   return (
-    <div className="flex gap-6">
+    <div className="flex flex-col sm:flex-row gap-6">
       <div className="flex flex-col gap-2">
         <h1 className="text-blue-500 text-5xl"> <Countup>12</Countup>  </h1>
         <p>Years Experience</p>
