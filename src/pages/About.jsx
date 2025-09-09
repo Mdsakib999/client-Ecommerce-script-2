aea59f62b4ee09ffdf5f75d38c5d8056ffd78739
MuntasirMahmud <muntasirm525@gmail.com>
2025-09-09 14:11:56 +0600

    about page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,11 @@
 import banner from "../assets/about-banner.jpg";
+import Countup from '../components/shared/Countup'
 import AboutSection from "../components/shared/AboutSection";
 export default function About() {
   return (
-    <div className="max-w-7xl mx-auto p-12">
+    <div className="">
       <div
-        className="relative mb-16 min-h-[600px] bg-no-repeat bg-cover bg-center flex items-center justify-center text-white"
+        className="relative mb-12 min-h-[600px] bg-no-repeat bg-cover bg-center flex items-center justify-center text-white"
         style={{ backgroundImage: `url(${banner})` }}
       >
         <div className="absolute inset-0 bg-black opacity-60"></div>
@@ -15,8 +16,44 @@ export default function About() {
           <p className="tracking-widest">WE CAN DO MORE FOR YOU</p>
         </div>
       </div>
+      <div className="px-4 max-w-7xl mx-auto text-center mb-12">
+        <p className="opacity-60">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div className="grid gap-6 grid-cols-4 mb-16 max-w-7xl mx-auto">
+        <div className="flex bg-gray-100 text-black flex-col gap-1 py-10 items-center justify-center text-center rounded-lg hover:bg-white hover:outline hover:outline-gray-600 transition">
+          <h1 className="text-4xl"> <Countup>800</Countup>+ </h1>
+          <p className="text-md">
+            Product Types
+          </p>
+        </div>
+        <div className="flex bg-gray-100 text-black flex-col gap-1 py-10 items-center justify-center text-center rounded-lg hover:bg-white hover:outline hover:outline-gray-600 transition">
+          <h1 className="text-4xl"> <Countup>12</Countup>+ </h1>
+          <p className="text-md">
+            Years Of Experiance
+          </p>
+        </div>
+        <div className="flex bg-gray-100 text-black flex-col gap-1 py-10 items-center justify-center text-center rounded-lg hover:bg-white hover:outline hover:outline-gray-600 transition">
+          <h1 className="text-4xl"> <Countup>2500</Countup>+ </h1>
+          <p className="text-md">
+            Trust Customers
+          </p>
+        </div>
+        <div className="flex bg-gray-100 text-black flex-col gap-1 py-10 items-center justify-center text-center rounded-lg hover:bg-white hover:outline hover:outline-gray-600 transition">
+          <h1 className="text-4xl"> <Countup>15</Countup>+ </h1>
+          <p className="text-md">
+            Stores Nationwide
+          </p>
+        </div>
+      </div>
       <AboutSection
-        className=""
         preheading="OUR HISTORY"
         heading="Creative and renovate fashion trends"
         normaltext="Collaboratively administrate empowered markets via plug-and-play maintain networks. Dynamically usable procrastinate B2B users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI."
