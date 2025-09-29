2c8b2faeaf40f9f542cb5387ff018f0c7a6ebb72
Md. Shahidul Islam <shanto57575@gmail.com>
2025-09-29 15:20:11 +0600

    updated multiple components

--- src/components/Home/PopularProduct.jsx
+++ src/components/Home/PopularProduct.jsx
@@ -3,8 +3,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import Loader from "../../utils/Loader";
 import { useGetAllProductQuery } from "../../redux/app/services/product/productApi";
-import { addToCart } from "../../redux/app/features/cart/cartSlice";
-import { useDispatch } from "react-redux";
+import Product from "../../pages/Products/Product";
 
 export default function PopularProduct() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -13,15 +12,12 @@ export default function PopularProduct() {
   useEffect(() => {
     const updateVisibleCount = () => {
       const width = window.innerWidth;
-      if (width >= 1280) setVisibleCount(4);
-      else if (width >= 1024) setVisibleCount(3);
-      else if (width >= 768) setVisibleCount(2);
-      else setVisibleCount(1);
+      if (width >= 1024) setVisibleCount(4); // large: 4
+      else if (width >= 768) setVisibleCount(2); // medium: 2
+      else setVisibleCount(1); // small: 1
     };
-
-    updateVisibleCount(); // initial call
+    updateVisibleCount();
     window.addEventListener("resize", updateVisibleCount);
-
     return () => window.removeEventListener("resize", updateVisibleCount);
   }, []);
 
@@ -33,12 +29,6 @@ export default function PopularProduct() {
     useGetAllProductQuery(params);
 
   const products = productsData?.data || [];
-
-  const dispatch = useDispatch();
-
-  const handleAddToCart = (product) => {
-    dispatch(addToCart(product));
-  };
 
   if (isProductLoading) return <Loader />;
 
@@ -76,70 +66,21 @@ export default function PopularProduct() {
         {/* Slider */}
         <div className="overflow-hidden">
           <div
-            className="flex transition-transform duration-700 ease-in-out"
+            className="flex transition-transform duration-700 ease-in-out mb-6"
             style={{
-              transform: `translateX(-${(currentIndex * 100) / visibleCount}%)`,
+              width: `${(products.length * 100) / visibleCount}%`,
+              transform: `translateX(-${
+                (currentIndex * 100) / products.length
+              }%)`,
             }}
           >
-            {products.map((product) => (
+            {products?.map((product) => (
               <div
-                key={product._id}
-                className="px-2 flex-shrink-0"
-                style={{ width: `${100 / visibleCount}%` }}
+                key={product?._id}
+                style={{ width: `${100 / products.length}%` }}
+                className="px-2 box-border"
               >
-                <div className="p-3">
-                  <div className="bg-white border border-gray-300 rounded-2xl shadow-md overflow-hidden relative transform transition-transform duration-300 hover:scale-105">
-                    {product.discountPrice && (
-                      <div className="absolute top-1 left-0 bg-red-500 text-white text-xs font-bold px-3 py-1 m-2 rounded-full">
-                        SALE
-                      </div>
-                    )}
-                    <div className="p-4 flex flex-col items-center">
-                      <div className="w-40 h-40 mb-4">
-                        <img
-                          src={product.images[0]}
-                          alt={product.name}
-                          className="w-full h-full rounded-full object-cover"
-                        />
-                      </div>
-                      <div className="w-full text-center">
-                        <p className="text-sm text-gray-500 mb-1">
-                          {product.category}
-                        </p>
-                        <h2 className="text-lg font-semibold text-gray-800 mb-2 ">
-                          <Link to={`/product/${product.id}`}>
-                            {product.name.slice(0, 25)}
-                          </Link>
-                        </h2>
-                        <div className="flex justify-center items-baseline space-x-2">
-                          {product.discountPrice ? (
-                            <>
-                              <p className="text-lg font-bold text-red-500">
-                                ${product.discountPrice}
-                              </p>
-                              <p className="text-sm text-gray-400 line-through">
-                                ${product.price}
-                              </p>
-                            </>
-                          ) : (
-                            <p className="text-lg font-bold text-gray-800">
-                              ${product.price}
-                            </p>
-                          )}
-                        </div>
-                        <div className="pt-3">
-                          <button
-                            onClick={() => handleAddToCart(product)}
-                            className="w-full  bg-gray-200 cursor-pointer rounded-full py-2 px-4 font-semibold text-sm flex items-center justify-center space-x-2 transition-colors hover:bg-gray-700 hover:text-white"
-                          >
-                            <ShoppingCart size={16} />
-                            <span>Add to Cart</span>
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <Product product={product} />
               </div>
             ))}
           </div>
